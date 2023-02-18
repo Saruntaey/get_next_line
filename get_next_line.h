b12b0619a1b327a24e9ad0bb8326b1bb7201eb00
Sarun Tapee <stapee@z3t8c2.42bangkok.com>
2023-02-18 17:14:12 +0700

    split func

--- get_next_line.h
+++ get_next_line.h
@@ -14,5 +14,8 @@ char *get_next_line(int fd);
 char *ft_concat_str(char *str1, char *str2);
 int ft_str_len(char *str);
 char *ft_str_clone(char *src);
+char *ft_str_clone_n(char *src, int n);
+int fill_line(int fd, char *bff, int bff_size, int *done);
+char *join_bff(char *str, char *bff, int bff_size);
 
 #endif
