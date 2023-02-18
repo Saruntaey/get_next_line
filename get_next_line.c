237c1517f7102b3f6e259ff695a91ba23f3168c1
Sarun Tapee <stapee@z3t8c2.42bangkok.com>
2023-02-18 17:39:09 +0700

    norm file

--- get_next_line.c
+++ get_next_line.c
@@ -1,12 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stapee <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/18 17:14:34 by stapee            #+#    #+#             */
+/*   Updated: 2023/02/18 17:38:57 by stapee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-
-char *do_get_next_line(int fd, char *bff, int bff_size)
+char	*do_get_next_line(int fd, char *bff, int bff_size)
 {
-	int n_read;
-	int done;
-	char *res;
-	char *res_tmp;
+	int		n_read;
+	int		done;
+	char	*res;
+	char	*res_tmp;
 
 	res = NULL;
 	done = 0;
@@ -14,83 +25,83 @@ char *do_get_next_line(int fd, char *bff, int bff_size)
 	{
 		n_read = fill_line(fd, bff, bff_size, &done);
 		if (n_read == -1)
-			return NULL;
+			return (NULL);
 		res_tmp = join_bff(res, bff, n_read);
 		if (!res_tmp)
-			return NULL;
+			return (NULL);
 		free(res);
 		res = res_tmp;
 	}
 	if (!ft_str_len(res))
 	{
 		free(res);
-		return NULL;
+		return (NULL);
 	}
-	return res;
+	return (res);
 }
 
-char *join_bff(char *str, char *bff, int bff_size)
+char	*join_bff(char *str, char *bff, int bff_size)
 {
-	char *res;
-	char *res_tmp;
+	char	*res;
+	char	*res_tmp;
 
 	if (!str)
 		str = "";
 	res_tmp = ft_str_clone_n(bff, bff_size);
 	if (!res_tmp)
-		return NULL;
+		return (NULL);
 	res = ft_concat_str(str, res_tmp);
 	free(res_tmp);
-	return res;
+	return (res);
 }
 
-int fill_line(int fd, char *bff, int bff_size, int *done)
+int	fill_line(int fd, char *bff, int bff_size, int *done)
 {
-	int i;
-	int n_read;	
+	int	i;
+	int	n_read;	
 
 	i = 0;
 	*done = 0;
 	while (i < bff_size)
 	{
-		n_read =  read(fd, &bff[i], 1);
+		n_read = read(fd, &bff[i], 1);
 		if (n_read == -1)
-			return -1;
+			return (-1);
 		i += n_read;
 		if (n_read == 0 || bff[i -1] == '\n')
 		{
 			*done = 1;
-			break;
+			break ;
 		}
 	}
-	return i;
+	return (i);
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-	char *bff;
-	char *line;
+	char	*bff;
+	char	*line;
 
 	bff = malloc(BUFFER_SIZE + 1);
 	if (!bff)
-		return NULL;
+		return (NULL);
 	line = do_get_next_line(fd, bff, BUFFER_SIZE + 1);
 	free(bff);
-	return line;	
+	return (line);
 }
 
-char *ft_concat_str(char *str1, char *str2)
+char	*ft_concat_str(char *str1, char *str2)
 {
-	int i;
-	int str1_len;
-	int str2_len;
-	char *res;
-	
+	int		i;
+	int		str1_len;
+	int		str2_len;
+	char	*res;
+
 	str1_len = ft_str_len(str1);
 	str2_len = ft_str_len(str2);
-	res = malloc(str1_len + str2_len + 1);	
+	res = malloc(str1_len + str2_len + 1);
 	if (!res)
-		return NULL;
+		return (NULL);
 	i = 0;
 	while (i < str1_len)
 	{
@@ -107,14 +118,14 @@ char *ft_concat_str(char *str1, char *str2)
 	return (res);
 }
 
-char *ft_str_clone_n(char *src, int n)
+char	*ft_str_clone_n(char *src, int n)
 {
-	char *res;
-	int i;
+	char	*res;
+	int		i;
 
-	res = malloc(n + 1);	
+	res = malloc(n + 1);
 	if (!res)
-		return NULL;
+		return (NULL);
 	i = 0;
 	while (i < n)
 	{
@@ -125,16 +136,16 @@ char *ft_str_clone_n(char *src, int n)
 	return (res);
 }
 
-char *ft_str_clone(char *src)
+char	*ft_str_clone(char *src)
 {
-	char *res;
-	int src_len;
-	int i;
+	char	*res;
+	int		src_len;
+	int		i;
 
 	src_len = ft_str_len(src);
-	res = malloc(src_len + 1);	
+	res = malloc(src_len + 1);
 	if (!res)
-		return NULL;
+		return (NULL);
 	i = 0;
 	while (i < src_len)
 	{
@@ -145,9 +156,9 @@ char *ft_str_clone(char *src)
 	return (res);
 }
 
-int ft_str_len(char *str)
+int	ft_str_len(char *str)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (str[i] != '\0')
